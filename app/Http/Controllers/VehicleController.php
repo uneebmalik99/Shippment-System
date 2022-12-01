a241574951641163ddb9c3d4bacfaf8f24472f99
Qkashif <qazikashif745@gmail.com>
2022-12-01 17:05:46 +0500

    by kashif

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -66,7 +66,6 @@ class VehicleController extends Controller
         $data['notification'] = Notification::with('user')->get();
         $data['location'] = Location::all()->toArray();
         // dd();
-        // kashif
         if ($data['notification']->toArray()) {
             $current = Carbon::now();
             foreach ($data['notification'] as $key => $date_notification) {
